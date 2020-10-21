576ba5823adb2c791355191faf812b1b003f5b48
Dejan Babić <babicaja@gmail.com>
2020-10-21 11:54:25 +0200

    Edit the metric threshold to 80

--- phpinsights.php
+++ phpinsights.php
@@ -96,10 +96,10 @@ return [
     */
 
     'requirements' => [
-        'min-quality' => 90,
-        'min-complexity' => 90,
-        'min-architecture' => 90,
-        'min-style' => 90,
+        'min-quality' => 80,
+        'min-complexity' => 80,
+        'min-architecture' => 80,
+        'min-style' => 80,
 //        'disable-security-check' => false,
     ],
 
